5e5f017f83d337ea388bf38ef21988529ac872c3
alzatin <alzatin@gmail.com>
2023-09-20 21:08:57 -0400

    clean

--- replicad-app-example/src/components/TopMenu.jsx
+++ replicad-app-example/src/components/TopMenu.jsx
@@ -10,7 +10,9 @@ function TopMenu() {
       'Read Me',
       'Bill of Materials',
       'Share',
-      'Save'
+      'Save',
+      'Pull Request',
+      'Open projects',
     ];
     /*{TO DO ::::make it if molecule is top then render goUpalEVELBUTTON otherwise dont}*/
    
@@ -39,25 +41,6 @@ function TopMenu() {
       <>
      <>{topLevel}</> 
      <Navbar />
-      
-    
-    {/*<div id="top_button_wrap">
-    <button className="top_buttons outerbutton" style= "right:40px;" type="button" id = "goup_top" title="Go up a level"> <img className="thumnail-logo" src='/imgs/goup_img.png' alt="Go-up" style="  transform: scale(.67); border-radius: 0px;"/> </button> 
-    <button className="top_buttons" type="button" id = "straight_menu" title="" style="background-color: #ededed; border: 2px solid black;"> 
-        <img className="three-menu" src='/imgs/three-menu.png' alt="three" /></button>
-    <div id ="toggle_wrap">
-        
-        <button className="top_buttons" type="button" id = "localMolecules_top" title="Available Molecules">  </button> 
-        <button className="top_buttons option" type="button" id = "delete_top" title="Delete Project"> <img className="thumnail-logo" src='/imgs/delete-logo.svg' alt="Delete Project" style=" transform: scale(.87); border-radius: 0px;"/> </button>
-         <button className="top_buttons option" type="button" id = "github_top" title="GitHub"> <img className="thumnail-logo" src='/imgs/github-logo.png' alt="Git" style=" transform: scale(1.2);" /> </button>
-         <button className="top_buttons option" type="button" id = "read_top" title="Read Me"> <img className="thumnail-logo" src='/imgs/read_img.png' alt="Read-me" style=" transform: scale(.67); border-radius: 0px;"/> </button> 
-         <button className="top_buttons option" type="button" id = "bom_top" title="Bill of Materials" > <img className="thumnail-logo" src='/imgs/bom-logo.png' alt="BOM"/> </button>
-         <button className="top_buttons option" type="button" id = "share_top" title="Share" > <img className="thumnail-logo" src='/imgs/share-img.png' alt="Share"/> </button>
-         <button className="top_buttons option" type="button" id = "save_top" title="Save"> <img className="thumnail-logo" src='/imgs/save-image.png' alt="Save" style=" transform: scale(.67); border-radius: 0px;"/> </button>  
-        <button className="top_buttons option" type="button" id = "projectmenu_top" title="Open projects"> <img className="thumnail-logo" src='/imgs/open-logo.png' alt="Load" style=" transform: scale(.67); border-radius: 0px;"/> </button>
-        <button className="top_buttons option" type="button" id = "pull_top" title="Make Pull Request"> <img className="thumnail-logo" src='/imgs/pullrequest-logo.png' alt="Make Pull Request" style=" transform: scale(.67); border-radius: 0px;"/> </button>
-    </div>
-</div>  */}
 </>
  )
 
