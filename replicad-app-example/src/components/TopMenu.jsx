951f871dbc84448793f68d7a8e5152c7fd4ff769
alzatin <alzatin@gmail.com>
2023-09-27 11:41:40 -0400

    lots of image updates to top menu

--- replicad-app-example/src/components/TopMenu.jsx
+++ replicad-app-example/src/components/TopMenu.jsx
@@ -4,19 +4,18 @@ import GlobalVariables from './js/globalvariables.js';
 
 function TopMenu() {
     const navItems = [
-      'Load',
-      'Delete Project',
+      'Open',
       'GitHub',
       'Read Me',
       'Bill of Materials',
       'Share',
-      'Save',
+      'Save Project',
       'Pull Request',
-      'Open',
+      'Delete Project'
     ];
     /*{TO DO ::::make it if molecule is top then render goUpalEVELBUTTON otherwise dont}*/
    
-    const topLevel= <img className="thumnail-logo navbar nav-img" src={'/imgs/goup_img.png'} key="" title="" />
+    const topLevel= <img className="nav-img nav-bar thumnail-logo" src={'/imgs/Go Up.svg'} key="" title="" />
     
     /*{nav bar toggle component}*/
     const Navbar = () => {
@@ -41,13 +40,15 @@ function TopMenu() {
       return (
         <>
         <nav ref={ref} className="navbar">
-          <button className="toggle nav-img" onClick={() => setNavbarOpen((prev) => !prev)} >
-          {navbarOpen ? <img className= "thumnail-logo" src={'/imgs/three-menu.png'} /> : <img className= "thumnail-logo" src={'/imgs/Save.png'} />}
+          <button className="toggle menu-nav-button" onClick={() => setNavbarOpen((prev) => !prev)} >
+          {navbarOpen ? <img className= "thumnail-logo nav-img" src={'/imgs/three-menu.svg'} /> : <img className= "thumnail-logo nav-img rotati" src={'/imgs/three-menu.svg'} />}
            </button>
 
           <ul className={`menu-nav${navbarOpen ? ' show-menu' : ''}`}> 
           {navItems.map((item, index) =>(
-            <img className=" thumnail-logo" src={'/imgs/'+item+'.svg'} key={item} title={item +"-button"} onClick={() => console.log({item})}/>
+            <button className='menu-nav-button'>
+            <img className=" thumnail-logo" alt={item} src={'/imgs/'+item+'.svg'} key={item} title={item +"-button"} onClick={() => console.log({item})}/>
+            </button>
         )) } 
     </ul>
     </nav>
