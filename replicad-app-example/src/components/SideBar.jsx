fe22e39151917dc10a20aa177512156f2b9a1693
alzatin <alzatin@gmail.com>
2023-11-21 15:13:54 -0500

    sidebar change and clean up

--- replicad-app-example/src/components/SideBar.jsx
+++ replicad-app-example/src/components/SideBar.jsx
@@ -2,14 +2,17 @@ import React, { useEffect, useState, useRef } from "react";
 import GlobalVariables from "./js/globalvariables.js";
 
 function SideBar(props) {
+  console.log("sidebar rerender");
   return (
     <>
       <div className="sideBar" value={GlobalVariables.currentRepo}>
-        {GlobalVariables.currentRepo
-          ? GlobalVariables.currentRepo.name +
-            " " +
-            GlobalVariables.currentRepo.owner.login
-          : null}
+        <p className="molecule_title">{GlobalVariables.currentRepo.name}</p>
+        <p className="atom_description">
+          {GlobalVariables.currentRepo.description}
+        </p>
+        <p>{GlobalVariables.atomsSelected}</p>
+
+        <ul className="sidebar-list"></ul>
       </div>
     </>
   );
