1c04050daca950d0fca3c3e2d6c45102b147e1df
alzatin <alzatin@gmail.com>
2024-01-03 17:22:47 -0500

    fixed overlapping menus with timeout

--- replicad-app-example/src/components/js/circular-menu/src/Creator/createAnchor.js
+++ replicad-app-example/src/components/js/circular-menu/src/Creator/createAnchor.js
@@ -4,125 +4,158 @@ import on from "./../on";
 import styleSheet from "./styleSheet";
 
 function hasSubMenus(menus) {
-    return menus instanceof Array && menus.length !== 0;
+  return menus instanceof Array && menus.length !== 0;
 }
-function ifDisabled(disabled){
-    if(disabled instanceof Function)
-        return disabled();
-    else
-        return Boolean(disabled);
+function ifDisabled(disabled) {
+  if (disabled instanceof Function) return disabled();
+  else return Boolean(disabled);
 }
 
-function setHref(ele, href){
-    if(!href) return;
+function setHref(ele, href) {
+  if (!href) return;
 
-    if(href instanceof Object){
-        ele.href = href.url;
-        ele.target = href.blank? "_blank" : "";
-    }else{
-        ele.href = href;
-    }
+  if (href instanceof Object) {
+    ele.href = href.url;
+    ele.target = href.blank ? "_blank" : "";
+  } else {
+    ele.href = href;
+  }
 }
 
-
-
-var delayShow = null;// delayShow reference the last setTimeout triggered by any one of menu item(anchor)
+var delayShow = null; // delayShow reference the last setTimeout triggered by any one of menu item(anchor)
 
 export default function (parent, data, index) {
-    var self = this;
+  var self = this;
 
-    var delayHide = null;// delayHide reference the last setTimeout triggered by the menu item itself
+  var delayHide = null; // delayHide reference the last setTimeout triggered by the menu item itself
 
-    var a = document.createElement('a');
+  var a = document.createElement("a");
 
-    setHref(a, data.href);
+  setHref(a, data.href);
 
-    a.setDisabled = function(){
-        classed(a, 'disabled', ifDisabled(data.disabled));
-    };
-    this._anchors.push(a);
+  a.setDisabled = function () {
+    classed(a, "disabled", ifDisabled(data.disabled));
+  };
+  this._anchors.push(a);
 
+  style(a, "width", this._calc.clickZoneSize.width);
+  style(a, "height", this._calc.clickZoneSize.height);
+  style(a, "right", this._calc.clickZoneSize.marginRight);
+  style(a, "bottom", this._calc.clickZoneSize.marginBottom);
+  style(
+    a,
+    "transform",
+    "skew(" +
+      -this._calc.skewDeg +
+      "deg) rotate(" +
+      this._calc.unskewDeg +
+      "deg) scale(1)"
+  );
 
-    style(a, 'width', this._calc.clickZoneSize.width);
-    style(a, 'height', this._calc.clickZoneSize.height);
-    style(a, 'right', this._calc.clickZoneSize.marginRight);
-    style(a, 'bottom', this._calc.clickZoneSize.marginBottom);
-    style(a, 'transform', 'skew(' + -this._calc.skewDeg + 'deg) rotate(' + this._calc.unskewDeg + 'deg) scale(1)');
+  classed(a, "disabled", ifDisabled(data.disabled));
 
-    classed(a, 'disabled', ifDisabled(data.disabled));
+  var percent = this._config.percent * 100 + "%";
+  styleSheet(
+    a,
+    "background",
+    "radial-gradient(transparent " +
+      percent +
+      ", " +
+      this._config.background +
+      " " +
+      percent +
+      ")"
+  );
+  styleSheet(
+    a,
+    "background",
+    "radial-gradient(transparent " +
+      percent +
+      ", " +
+      this._config.backgroundHover +
+      " " +
+      percent +
+      ")",
+    "hover"
+  );
 
+  function clickCallBack(e, data) {
+    if (data.click) data.click.call(this, e, data);
 
-    var percent = this._config.percent * 100 + "%";
-    styleSheet(a, 'background', 'radial-gradient(transparent ' + percent + ', ' + this._config.background + ' ' + percent + ')');
-    styleSheet(a, 'background', 'radial-gradient(transparent ' + percent + ', ' + this._config.backgroundHover + ' ' + percent + ')', 'hover');
+    if (self._config.hideAfterClick) {
+      self._cMenu.hide();
+      if (self._cMenu._pMenu) self._cMenu._pMenu.hide();
+      if (subMenu) subMenu.hide();
+    }
+  }
 
+  // this is where the tooltip div is created to show names of elements in circular menu
+  on(a, "mouseenter", function () {
+    var div = document.createElement("div");
+    div.textContent = data.icon;
+    div.classList.add("tooltip");
+    div.id = data.icon + "text";
+    const length = div.textContent.length * 2.7; //Correct for text length centering
+    document.querySelector("body").appendChild(div);
+    style(
+      div,
+      "top",
+      self._container.offsetTop + self._calc.radius - 10 + "px"
+    );
+    style(
+      div,
+      "left",
+      self._container.offsetLeft + self._calc.radius - length + "px"
+    );
+  });
 
-    function clickCallBack(e, data){
-        if (data.click) data.click.call(this, e, data);
+  on(a, "click", clickCallBack, data);
+  on(a, "mouseleave", function () {
+    document.getElementById(data.icon + "text").remove();
+  });
 
-        if(self._config.hideAfterClick){
-            self._cMenu.hide();
-            if(self._cMenu._pMenu) self._cMenu._pMenu.hide();
-            if(subMenu) subMenu.hide();
+  parent.appendChild(a);
+
+  this._createHorizontal(a, data, index);
+
+  //toggle subMenu
+  if (hasSubMenus(data.menus)) {
+    var subMenu = this._createSubMenu(self, data.menus, index);
+    let hovered = false;
+    on(a, "mouseenter", function () {
+      hovered = true;
+      delayShow = setTimeout(function () {
+        if (hovered) {
+          subMenu
+            .styles({
+              top: self._container.offsetTop + self._calc.radius + "px",
+              left: self._container.offsetLeft + self._calc.radius + "px",
+            })
+            .show();
         }
-    }
+        //console.log(hovered);
+      }, 500);
+    });
 
-    // this is where the tooltip div is created to show names of elements in circular menu
-    on(a, 'mouseenter', function () {
-        var div = document.createElement('div');
-        div.textContent = data.icon
-        div.classList.add("tooltip");
-        div.id = data.icon + "text"
-        const length= div.textContent.length * 2.7 //Correct for text length centering 
-        document.querySelector("body").appendChild(div)
-        style(div, 'top', self._container.offsetTop + self._calc.radius - 10 + 'px');
-        style(div, 'left', self._container.offsetLeft + self._calc.radius - length + 'px');
-        });
+    on(a, "mouseleave", function (e) {
+      if (!subMenu._container.contains(e.toElement)) {
+        hovered = false;
+        delayHide = setTimeout(function () {
+          subMenu.hide();
+        }, 100);
+      }
+    });
 
-    on(a, 'click', clickCallBack, data);
-    on(a, 'mouseleave', function () {
-            document.getElementById(data.icon + "text").remove()
-            });
+    on(subMenu._container, "mouseenter", function () {
+      clearTimeout(delayShow);
+      clearTimeout(delayHide);
+    });
 
-    parent.appendChild(a);
-
-    this._createHorizontal(a, data, index);
-    
-    
-    //toggle subMenu
-    if (hasSubMenus(data.menus)) {
-        var subMenu = this._createSubMenu(self, data.menus, index);
-
-        on(a, 'mouseenter', function () {
-            delayShow = setTimeout(function () {
-                subMenu
-                    .styles({
-                                top: self._container.offsetTop + self._calc.radius + 'px',
-                                left: self._container.offsetLeft + self._calc.radius + 'px'
-                            })
-                    .show();
-            }, 350);
-        });
-
-        on(a, 'mouseleave', function (e) {
-            if (!subMenu._container.contains(e.toElement)) {
-                delayHide = setTimeout(function () {
-                    subMenu.hide();
-                }, 200);
-            }
-        });
-
-        on(subMenu._container, 'mouseenter', function () {
-            clearTimeout(delayShow);
-            clearTimeout(delayHide);
-        });
-
-       
-
-        on(subMenu._container, 'mouseleave', function (e) {
-            if (!a.contains(e.toElement) || e.toElement.children[0] === a) {
-                subMenu.hide();
-            }
-        });
-    }
+    on(subMenu._container, "mouseleave", function (e) {
+      hovered = false;
+      if (!a.contains(e.toElement) || e.toElement.children[0] === a) {
+        subMenu.hide();
+      }
+    });
+  }
 }
--- replicad-app-example/src/components/js/circular-menu/src/show.js
+++ replicad-app-example/src/components/js/circular-menu/src/show.js
@@ -1,29 +1,29 @@
 import classed from "./classed";
 import style from "./style";
 
-function setCoordinate(coordinate){
-    if( !(coordinate instanceof Array) || !(coordinate.length === 2) ) return;
+function setCoordinate(coordinate) {
+  if (!(coordinate instanceof Array) || !(coordinate.length === 2)) return;
 
-    //TODO verify if has unit
-    style(this._container, 'left', coordinate[0] + "px");
-    style(this._container, 'top', coordinate[1] + "px");
+  //TODO verify if has unit
+  style(this._container, "left", coordinate[0] + "px");
+  style(this._container, "top", coordinate[1] + "px");
 }
 
 //check disabled
 
-function setDisabled(){
-    this._creator._anchors.forEach(function(v){
-        v.setDisabled();
-    });
+function setDisabled() {
+  this._creator._anchors.forEach(function (v) {
+    v.setDisabled();
+  });
 }
 
 export default function (coordinate) {
+  setDisabled.call(this);
 
+  setCoordinate.call(this, coordinate);
 
-    setDisabled.call(this);
-
-    setCoordinate.call(this, coordinate);
-
-    classed(this._container, 'opened-nav', true);
-    return this;
+  classed(this._container, "opened-nav", true);
+  console.log(this);
+  console.log(this._container);
+  return this;
 }
