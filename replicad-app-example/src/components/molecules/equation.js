9841099567e2d5b4afaeed85bde5a2351f8e2f89
alzatin <alzatin@gmail.com>
2024-01-31 10:31:56 -0500

    uniqueID

--- replicad-app-example/src/components/molecules/equation.js
+++ replicad-app-example/src/components/molecules/equation.js
@@ -240,6 +240,6 @@ export default class Equation extends Atom {
   sendToRender() {
     //Send code to jotcad to render
 
-    GlobalVariables.writeToDisplay(this.path);
+    GlobalVariables.writeToDisplay(this.uniqueID);
   }
 }
