0e15034753f04a89564e8459d76cff8bdc23762d
alzatin <47953734+alzatin@users.noreply.github.com>
2024-07-13 12:36:43 -0400

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -297,11 +297,12 @@ function shrinkWrapSketches(targetID, inputIDs) {
         BOM.push(library[inputID].bom);
       });
       let geometryToWrap = chainFuse(inputsToFuse);
+      const newPlane = new Plane().pivot(0, "Y");
       library[targetID] = {
         geometry: [shrinkWrap(geometryToWrap, 50)],
         tags: [],
         color: "#FF9065",
-        plane: "XY",
+        plane: newPlane,
         bom: BOM,
       };
       return true;
@@ -522,7 +523,6 @@ async function importingSVG(targetID, file, depth, width) {
       color: "#FF9065",
       bom: [],
     };
-    console.log(library[targetID]);
     return true;
   });
 }
@@ -813,8 +813,6 @@ function recursiveCut(partToCut, cuttingPart) {
       return cutPart;
     }
   } catch (e) {
-    console.error("Recursive Cut failed", partToCut, cuttingPart);
-
     throw new Error("Recursive Cut failed");
   }
 }
@@ -942,7 +940,6 @@ function chainFuse(chain) {
     }
     return fused;
   } catch (e) {
-    console.error(chain);
     throw new Error("Fusion failed");
   }
 }
@@ -994,7 +991,6 @@ let colorOptions = {
 
 function generateDisplayMesh(id) {
   return started.then(() => {
-    console.log(library[id]);
     if (library[id] == undefined) {
       throw new Error("ID not found in library");
     }
