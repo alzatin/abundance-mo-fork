5b98c957cfca9d8b1814503152d460eea039ee8d
alzatin <47953734+alzatin@users.noreply.github.com>
2024-07-22 14:49:04 -0400

    add export button

--- src/components/secondary/RunNavigation.jsx
+++ src/components/secondary/RunNavigation.jsx
@@ -114,6 +114,20 @@ let billSvg = (
     />
   </svg>
 );
+let exportSvg = (
+  <svg
+    width="30px"
+    height="30px"
+    fill="none"
+    viewBox="0 0 23 23"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill="#c4a3d5"
+      d="M8.71,7.71,11,5.41V15a1,1,0,0,0,2,0V5.41l2.29,2.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42l-4-4a1,1,0,0,0-.33-.21,1,1,0,0,0-.76,0,1,1,0,0,0-.33.21l-4,4A1,1,0,1,0,8.71,7.71ZM21,14a1,1,0,0,0-1,1v4a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V15a1,1,0,0,0-2,0v4a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V15A1,1,0,0,0,21,14Z"
+    />
+  </svg>
+);
 
 function RunNavigation(props) {
   let [shareDialog, setShareDialog] = useState(false);
@@ -238,6 +252,16 @@ function RunNavigation(props) {
           }}
         >
           {starSvg}
+        </button>
+        <button
+          onClick={() => {
+            console.log("export");
+            //setShareDialog(true);
+          }}
+          className=" run-navigation-button"
+          id="Export-button"
+        >
+          {exportSvg}
         </button>
         <button
           className=" run-navigation-button"
