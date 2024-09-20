f8f03cdf22095ba53f64c98ff0674633a18c87b0
alzatin <47953734+alzatin@users.noreply.github.com>
2024-09-20 13:46:01 -0400

    updated all items to include lowercase name in searchfield

--- Lambda AWS Functions/updateAllItems/lambda_function.py
+++ Lambda AWS Functions/updateAllItems/lambda_function.py
@@ -4,51 +4,50 @@ import os
 import json
 import datetime
 
-def lambda_handler(event:any, context:any):
-    
+
+def lambda_handler(event: any, context: any):
+
     returnObject = {}
-    
-    #create a dynamodb client
+
+    # create a dynamodb client
     dynamodb = boto3.resource("dynamodb")
-    #get the item from the table
+    # get the item from the table
     table_name = os.environ["TABLE_NAME"]
     table = dynamodb.Table(table_name)
-  
-    
+
     item_array = []
     response = table.scan()
     item_array.extend(response.get('Items', []))
-    
+
     newDate = datetime.datetime.now().strftime('%m/%d/%Y')
     yearNow = datetime.datetime.now().year
     updateExpressions = []
-    
-    
-    expressionAttributeValues = {
-            ':dateModified': newDate,
-            ':yyyy': yearNow,
-        }
-   
+
     for item in item_array:
         print(item)
         owner = item['owner']
         repoName = item['repoName']
+        repoNameLower = repoName.lower()
+
+        expressionAttributeValues = {
+            ':dateModified': newDate,
+            ':yyyy': yearNow,
+            ':searchField': repoNameLower,
+
+        }
+
         response = table.update_item(
             Key={
                 'owner': owner,
                 'repoName': repoName
             },
-            UpdateExpression='SET dateModified= :dateModified, yyyy = :yyyy', 
-            ExpressionAttributeValues= expressionAttributeValues
+            UpdateExpression='SET dateModified= :dateModified, yyyy = :yyyy, searchField = :searchField',
+            ExpressionAttributeValues=expressionAttributeValues
         )
-    
-    returnObject['statusCode']= 200
-    returnObject['headers'] = {}
-    returnObject["body"]= json.dumps("hello")
-    returnObject['headers']['Content-Type'] = 'application/json'
-    
-    return returnObject
 
-    
-    
-    
+    returnObject['statusCode'] = 200
+    returnObject['headers'] = {}
+    returnObject["body"] = json.dumps("hello")
+    returnObject['headers']['Content-Type'] = 'application/json'
+
+    return returnObject
