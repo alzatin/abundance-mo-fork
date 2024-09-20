a5f2637dffeded4459c2731460cfd3ab9df2f004
alzatin <47953734+alzatin@users.noreply.github.com>
2024-09-20 13:00:06 -0400

    fix reload for forked

--- Lambda AWS Functions/post-new-project-abundance/lambda_function.py
+++ Lambda AWS Functions/post-new-project-abundance/lambda_function.py
@@ -3,45 +3,60 @@ import os
 import json
 from datetime import datetime
 
-def lambda_handler(event:any, context:any):
-    
-    
+
+def lambda_handler(event: any, context: any):
+
     returnObject = {}
-    
-    #create a dynamodb client
+
+    # create a dynamodb client
     dynamodb = boto3.resource("dynamodb")
-    #get the item from the table
+    # get the item from the table
     table_name = os.environ["TABLE_NAME"]
     table = dynamodb.Table(table_name)
-    
+
     print(event)
-    
+
     try:
         owner = event["owner"]
         repoName = event["repoName"]
-        ranking =event["ranking"]
+        ranking = event["ranking"]
         forks = event["forks"]
-        topMoleculeID =event["topMoleculeID"]
+        topMoleculeID = event["topMoleculeID"]
         topics = event["topics"]
         readMe = event["readme"]
-        contentURL =event["contentURL"]
-        svgURL =event["svgURL"]
-        dateCreated = event["dateCreated"]   
-        
+        contentURL = event["contentURL"]
+        svgURL = event["svgURL"]
+        dateCreated = event["dateCreated"]
+        parentRepo = event["parentRepo"]
+        githubMoleculesUsed = event["githubMoleculesUsed"]
+
         newYear = datetime.now().year
         print(newYear)
-          
-        table.put_item(Item={"owner": owner, "repoName": repoName, "ranking":ranking,"forks": forks, "topMoleculeID": topMoleculeID, "svgURL": svgURL, "topics":topics, "readMe":readMe,"dateCreated":dateCreated, "contentURL":contentURL, "yyyy": newYear})
-        returnObject['statusCode']= 200
-        message = "Success put item"  
-        
+
+        itemToPut = {"owner": owner,
+                     "repoName": repoName,
+                     "ranking": ranking,
+                     "forks": forks,
+                     "topMoleculeID": topMoleculeID,
+                     "svgURL": svgURL,
+                     "topics": topics,
+                     "readMe": readMe,
+                     "dateCreated": dateCreated,
+                     "contentURL": contentURL,
+                     "parentRepo": parentRepo,
+                     "githubMoleculesUsed": githubMoleculesUsed,
+                     "yyyy": newYear}
+
+        table.put_item(Item=itemToPut)
+        returnObject['statusCode'] = 200
+        message = "Success put item"
+
     except:
-        returnObject['statusCode']= 400
-        message= 'no'
-         
-   
+        returnObject['statusCode'] = 400
+        message = 'no'
+
     returnObject['headers'] = {}
-    returnObject["body"]= json.dumps("hello")
+    returnObject["body"] = json.dumps(message)
     returnObject['headers']['Content-Type'] = 'application/json'
-    
+
     return returnObject
