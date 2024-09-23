980cbcc7ad798073b0a19d9f8320ca88d8cc4a93
alzatin <47953734+alzatin@users.noreply.github.com>
2024-09-23 13:33:33 -0400

    initially hide shortcuts

--- src/js/globalvariables.js
+++ src/js/globalvariables.js
@@ -266,7 +266,7 @@ class GlobalVariables {
      * A flag to indicate if shortcuts should be displayed.
      * @type {boolean}
      */
-    this.displayShortcuts = true;
+    this.displayShortcuts = false;
     /**
      * A flag to indicate if the faces of the shape should be displayed.
      * @type {boolean}
