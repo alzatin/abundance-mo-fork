7c095d4aaf6bb5da996e76d2602f326c45857801
alzatin <47953734+alzatin@users.noreply.github.com>
2024-09-29 11:22:17 -0400

    Update lambdanode.js

--- lambdaToGit/lambdanode.js
+++ lambdaToGit/lambdanode.js
@@ -4,11 +4,10 @@ import {
   DynamoDBDocumentClient,
   ScanCommand,
   DeleteCommand,
-  UpdateItemCommand,
+  UpdateCommand,
 } from "@aws-sdk/lib-dynamodb";
 
 const client = new DynamoDBClient({});
-
 const dynamo = DynamoDBDocumentClient.from(client);
 
 const tableName = "abundance-projects";
@@ -16,6 +15,7 @@ const tableName = "abundance-projects";
 export const handler = async (event, context) => {
   const octokit = new Octokit();
 
+  /*Scans parameter to returns attributes owner, repoName, fork from all repositories in table*/
   const command = new ScanCommand({
     ProjectionExpression: "#ow, #repoName, #forks",
     ExpressionAttributeNames: {
@@ -31,6 +31,7 @@ export const handler = async (event, context) => {
   let promises = tableItems.Items.map((repo) => {
     return checkGithub(repo.owner, repo.repoName, repo.forks); // Added return statement
   });
+
   await Promise.all(promises).then((results) => {
     const response = {
       statusCode: 200,
@@ -40,24 +41,40 @@ export const handler = async (event, context) => {
   });
 
   async function checkUpdate(owner, repoName, forks, githubForks) {
+    console.log(owner, repoName, forks, githubForks);
     if (forks !== githubForks) {
-      const params = {
-        TableName: tableName,
+      console.log("updating forks");
+      const input = {
+        ExpressionAttributeNames: {
+          "#forks": "forks",
+        },
+        ExpressionAttributeValues: {
+          ":forks": {
+            N: githubForks,
+          },
+        },
+        ReturnValues: "ALL_NEW",
+        TableName: "abundance-projects",
+        UpdateExpression: "SET #forks = :forks",
         Key: {
           owner: owner,
           repoName: repoName,
         },
-        UpdateExpression: "SET forks = :forks",
-        ExpressionAttributeValues: {
-          ":forks": githubForks,
-        },
       };
-      const command = new UpdateItemCommand(params);
-      const responseUpdate = await client.send(command);
-      return responseUpdate;
+      const command = new UpdateCommand(input);
+      dynamo
+        .send(command)
+        .then((response) => {
+          console.log(response);
+          return response;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     }
   }
 
+  /* Makes request to github to check if repo exists, if it doesn't deletes from table, it it does updates in table*/
   async function checkGithub(owner, repoName, forks) {
     await octokit.rest.repos
       .get({
@@ -65,9 +82,13 @@ export const handler = async (event, context) => {
         repo: repoName,
       })
       .then((response) => {
-        console.log(response.status);
-        checkUpdate(owner, repoName, forks, response.data.forks_count);
-        return response.status;
+        //console.log(repoName);
+        checkUpdate(owner, repoName, forks, response.data.forks_count).then(
+          (response) => {
+            console.log(response);
+            return response;
+          }
+        );
       })
       .catch((error) => {
         console.log(`${repoName} does not exist`);
@@ -76,6 +97,7 @@ export const handler = async (event, context) => {
         return;
       });
   }
+  /*Removes non existent repos from table */
   async function deleteFromTable(owner, repoName) {
     const params = {
       TableName: tableName,
@@ -89,6 +111,5 @@ export const handler = async (event, context) => {
     const response1 = await dynamo.send(command);
     console.log("deleting item");
     console.log(repoName);
-    console.log(response1);
   }
 };
