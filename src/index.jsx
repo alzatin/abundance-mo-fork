9641d9e79fdde6b4ff04421c283d0d16de3dc36f
alzatin <47953734+alzatin@users.noreply.github.com>
2024-10-28 15:50:00 -0400

    hashrouter test

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { HashRouter } from "react-router-dom";
 
 // This is here to compensate for a bug in vite
 import "replicad-opencascadejs/src/replicad_single.wasm?url";
@@ -8,7 +9,9 @@ import "replicad-opencascadejs/src/replicad_single.wasm?url";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
