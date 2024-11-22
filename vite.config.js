b298064148a8656603f4038337cde37538703c18
alzatin <47953734+alzatin@users.noreply.github.com>
2024-11-22 17:46:26 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import reactPlugin from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactPlugin()],
+<<<<<<< Updated upstream
   base: "/",
+=======
+  base: "/abundance-mo-fork/ ",
+>>>>>>> Stashed changes
   build: {
     outDir: "dist",
   },
