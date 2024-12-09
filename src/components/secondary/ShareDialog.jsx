b73ee09ffc1b813d05b48972755142a2e03360f7
alzatin <47953734+alzatin@users.noreply.github.com>
2024-12-08 20:30:34 -0500

    update share project link

--- src/components/secondary/ShareDialog.jsx
+++ src/components/secondary/ShareDialog.jsx
@@ -35,7 +35,7 @@ function ShareDialog({
       >
         {dialogContent == "share" ? (
           <div style={{ display: "flex", margin: "10px" }}>
-            <p>Use this link to share this project:</p>
+            <p>Share this project:</p>
             <a
               style={{ margin: "16px" }}
               href={
@@ -47,7 +47,7 @@ function ShareDialog({
               }
               target="_blank"
             >
-              /run/{GlobalVariables.currentRepo.owner}/
+              {window.location.origin}/run/{GlobalVariables.currentRepo.owner}/
               {GlobalVariables.currentRepo.repoName}
             </a>
           </div>
