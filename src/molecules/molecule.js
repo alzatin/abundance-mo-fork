eafa0978d6398569c98d0b7def4b1885246e1b3d
alzatin <47953734+alzatin@users.noreply.github.com>
2025-01-25 14:00:20 -0500

    Update molecule.js

--- src/molecules/molecule.js
+++ src/molecules/molecule.js
@@ -892,7 +892,6 @@ export default class Molecule extends Atom {
    * @param {boolean} unlock - A flag to indicate if this atom should spawn in the unlocked state.
    */
   async placeAtom(newAtomObj, unlock, values) {
-    console.log(newAtomObj);
     try {
       GlobalVariables.numberOfAtomsToLoad =
         GlobalVariables.numberOfAtomsToLoad + 1; //Indicate that one more atom needs to be loaded
