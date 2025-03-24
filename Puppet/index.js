cd40598f6af20da1d159e0d9c5945e81ae9296b0
alzatin <47953734+alzatin@users.noreply.github.com>
2025-03-24 16:45:31 -0400

    Update index.js

--- Puppet/index.js
+++ Puppet/index.js
@@ -14,76 +14,69 @@ const currentDate = new Date().toISOString().split("T")[0];
 //for each project in projects to test launch puppeteer
 
 (async () => {
-  try {
-    await loadPuppeteerAndExec(currentDate);
-  } catch (error) {
-    console.error(`Error processing projects`);
-  }
-  console.log("All projects processed:!");
-})();
-
-async function loadPuppeteerAndExec(date) {
   let browser;
   try {
     browser = await puppeteer.launch({
       headless: true,
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
-    const page = await browser.newPage();
-
-    for (const projectName of projects_to_test) {
-      console.log(projectName);
-      // Navigate the page to a localhost URL.
-      await page.goto(
-        "http://localhost:4444" + "/run/" + projectUser + "/" + projectName
-      );
-      console.log("navigated to: " + projectName);
-      // Set screen size.
-      await page.setViewport({ width: 1080, height: 1024 });
-      const selector = "#molecule-fully-render-puppeteer";
-      /*await page.waitForSelector("#molecule-fully-render-puppeteer", {
-      visible: true,
-      timeout: 120000,
-    });*/
-      await page.waitForFunction(
-        (selector) => !!document.querySelector(selector),
-        { timeout: 120000 }, // Increase timeout to 2 minutes
-        selector
-      );
-      await page.screenshot({
-        path: `Puppet/images/${projectName}-Test-${date}.png`,
-      });
-      console.log(`Screenshot: Puppet/images/${projectName}-Test-${date}.png `);
-
-      await page.goto(
-        "https://barboursmith.github.io/Abundance" +
-          "/run/" +
-          projectUser +
-          "/" +
-          projectName
-      );
-      await page.waitForFunction(
-        (selector) => !!document.querySelector(selector),
-        { timeout: 120000 }, // Increase timeout to 2 minutes
-        selector
-      );
-      await page.screenshot({
-        path: `Puppet/images/${projectName}-Deployed-${date}.png`,
-      });
-
-      console.log(
-        `Screenshot: Puppet/images/${projectName}-Deployed-${date}.png`
-      );
-    }
+    await loadPuppeteerAndExec(browser, currentDate);
   } catch (error) {
-    console.error(
-      `Error in loadPuppeteerAndExec for project ${projectName}:`,
-      error
-    );
+    console.error(`Error processing projects`);
   } finally {
     if (browser) {
       console.log(`Closing browser for project ${projectName}`);
       await browser.close();
     }
+  }
+  console.log("All projects processed:!");
+})();
+
+async function loadPuppeteerAndExec(browser, date) {
+  const page = await browser.newPage();
+
+  for (const projectName of projects_to_test) {
+    console.log(projectName);
+    // Navigate the page to a localhost URL.
+    await page.goto(
+      "http://localhost:4444" + "/run/" + projectUser + "/" + projectName
+    );
+    console.log("navigated to: " + projectName);
+    // Set screen size.
+    await page.setViewport({ width: 1080, height: 1024 });
+    const selector = "#molecule-fully-render-puppeteer";
+    /*await page.waitForSelector("#molecule-fully-render-puppeteer", {
+      visible: true,
+      timeout: 120000,
+    });*/
+    await page.waitForFunction(
+      (selector) => !!document.querySelector(selector),
+      { timeout: 120000 }, // Increase timeout to 2 minutes
+      selector
+    );
+    await page.screenshot({
+      path: `Puppet/images/${projectName}-Test-${date}.png`,
+    });
+    console.log(`Screenshot: Puppet/images/${projectName}-Test-${date}.png `);
+
+    await page.goto(
+      "https://barboursmith.github.io/Abundance" +
+        "/run/" +
+        projectUser +
+        "/" +
+        projectName
+    );
+    await page.waitForFunction(
+      (selector) => !!document.querySelector(selector),
+      { timeout: 120000 }, // Increase timeout to 2 minutes
+      selector
+    );
+    await page.screenshot({
+      path: `Puppet/images/${projectName}-Deployed-${date}.png`,
+    });
+
+    console.log(
+      `Screenshot: Puppet/images/${projectName}-Deployed-${date}.png`
+    );
   }
 }
